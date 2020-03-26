dbf642321a0afbddaa3d42d3127480af2c475abc
Xiang Hao <karta2066274@gmail.com>
2020-03-26 17:56:27 +0800

    Register half

--- shop2/routes/web.php
+++ shop2/routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/register', function(){
+	return view('register');
+});
+
+Route::post('/postReg', 'UserController@Register');
